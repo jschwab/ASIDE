4cf9f77cc9864eb1a544301b1cb8227863e0d9a8
Josiah Schwab <jwschwab@berkeley.edu>
2011-10-31 15:46:16 -0700

    add module for generic drifting

---
+++ drift.F90
@@ -0,0 +1,30 @@
+MODULE drift
+       
+  USE types
+
+  CONTAINS
+
+  SUBROUTINE drift_kep(dt, p)
+
+    USE kepler
+
+    ! dt - integrator timestep
+    ! p  - particle to drift
+
+    REAL(rl), INTENT(IN) :: dt
+    TYPE(particle), INTENT(INOUT) :: p
+    
+    REAL(rl), DIMENSION(3) :: x, v
+    REAL(rl) :: f, g, fdot, gdot
+
+    CALL gaussfg(dt, p, f, g, fdot, gdot)
+
+    x = (p % x) * f    + (p % v) * g
+    v = (p % x) * fdot + (p % v) * gdot
+
+    p % x = x
+    p % v = v
+
+  END SUBROUTINE drift_kep
+
+END MODULE drift
